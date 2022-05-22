f5ad9f3642f26ae88baee484ff899a13d42707ef
Lorenz Grohmann <lorenz.grohmann@gmx.de>
2022-05-22 20:59:19 +0200

    initial commit

---
+++ client.py
@@ -0,0 +1,143 @@
+import io
+import socket
+import struct
+import time
+import logging
+import os
+import picamera
+from datetime import date
+from PIL import Image
+
+from conf_client import *
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+failed_upload = 2147483647
+
+
+def send_image(data, size, date):
+    result = False
+    try:
+        client_socket = socket.socket()
+        client_socket.connect((SERVER_ADDR, SERVER_PORT))
+        connection = client_socket.makefile("wb")
+
+        collection_data = TIMELAPS_NAME.encode()
+        connection.write(struct.pack('<L', len(collection_data)))
+        connection.flush()
+        connection.write(collection_data)
+
+        connection.write(struct.pack('<L', size))
+        connection.write(struct.pack('<L', date))
+        connection.flush()
+
+        connection.write(data)
+        connection.write(struct.pack('<L', 0))
+        client_socket.close()
+        result =  True
+    finally:
+        client_socket.close()
+        return result
+
+def safe_image(data, date):
+    img = Image.open(io.BytesIO(data))
+    path = LOCAL_STORAGE_PATH + "/%s_%s.jpeg" % (TIMELAPS_NAME,date)
+    img.save(path, "JPEG")
+    logger.info("Photo Saved to %s" % path)
+
+
+def uplaod_local_pictures():
+    result = False
+    try:
+        client_socket = socket.socket()
+        client_socket.connect((SERVER_ADDR, SERVER_PORT))
+        connection = client_socket.makefile("wb")
+        collection_data = TIMELAPS_NAME.encode()
+        connection.write(struct.pack('<L', len(collection_data)))
+        connection.flush()
+        connection.write(collection_data)
+
+        for file in os.listdir(LOCAL_STORAGE_PATH):
+            path = LOCAL_STORAGE_PATH + "/" + file
+            date = int(file.replace(".jpeg","").split("_")[1])
+            image = Image.open(path)
+            image.verify()
+            image = Image.open(path)
+            data = io.BytesIO()
+            image.save(data, format="JPEG")
+            size = data.getbuffer().nbytes
+
+            connection.write(struct.pack('<L', size))
+            connection.flush()
+            connection.write(struct.pack('<L', date))
+            connection.flush()
+            data.seek(0)
+            connection.write(data.read())
+            os.remove(path) 
+            logger.info("Uploaded %s" % file)
+        
+        connection.write(struct.pack('<L', 0))
+        client_socket.close()
+        result = True
+        time.sleep(0.2)
+    finally:
+        client_socket.close()
+        return result
+
+
+
+def main():
+    global failed_upload
+    camera = picamera.PiCamera()
+    camera.resolution = (1280, 720)
+    # Start a preview and let the camera warm up for 2 seconds
+    camera.start_preview()
+    time.sleep(2)
+    stream = io.BytesIO()
+    if len(os.listdir(LOCAL_STORAGE_PATH)) !=0:
+        logger.info("%s Local Pictures Present.. Attempting Upload!" % len(os.listdir(LOCAL_STORAGE_PATH)))
+        if uplaod_local_pictures():
+            logger.info("Upload Succesfull!")
+        else:
+            logger.info("Upload of Local Pictures Failed! Attempting again in 10 minutes")
+            failed_upload = int(time.time()) + 600
+
+
+    for foo in camera.capture_continuous(stream, 'jpeg'):
+        image_date = int(time.time())
+        image_size = stream.tell()
+        stream.seek(0)
+        image_data = stream.read()
+
+        stream.seek(0)
+        stream.truncate()
+
+        if failed_upload < int(time.time()):
+            logger.info("Attempting Uplaod of Local Pictures")
+            if uplaod_local_pictures():
+                failed_upload = 2147483647
+                logger.info("Upload Succesfull!")
+            else:
+                logger.info("Upload of local pictures Failed! Attempting again in 10 minutes")
+                failed_upload = int(time.time()) + 600
+
+
+        if failed_upload == 2147483647:
+            if not send_image(image_data, image_size, image_date):
+                logger.info("Upload of Picture Failed! Pictures will be stored locally and a collective upload will be attempted in 10 Minutes")
+                safe_image(image_data, image_date)
+                failed_upload = int(time.time()) + 600
+            else:
+                logger.info("Picture Uploaded")
+        else:
+            safe_image(image_data, image_date)#
+
+        stream.seek(0)
+        stream.truncate()
+        time.sleep(TIMELAPSE_INTERVAL)
+
+main()
---
+++ server.py
@@ -0,0 +1,106 @@
+import logging
+import io
+import socket
+import struct
+import _thread
+from PIL import Image
+from telegram.ext import Updater, CommandHandler
+from pathlib import Path
+from datetime import datetime
+
+from conf_server import *
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+telegram_count = {}
+
+def get_id(update, context):
+    user = update["_effective_user"]["id"]
+    context.bot.send_message(update["message"]["chat"]["id"], str(user))
+
+
+def on_new_client(clientsocket, addr):
+    file = clientsocket.makefile()
+    logger.info("New Connection form " + str(addr[0]))
+    connection = clientsocket.makefile("rwb")
+    try:
+        collection_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        collection = connection.read(collection_len).decode()
+        if not collection in telegram_count:
+            telegram_count[collection] = TELEGRAM_SEND_INTERVAL
+
+        while True:
+            image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
+                break
+            image_date = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            print(1)
+
+            image_stream = io.BytesIO()
+            image_stream.write(connection.read(image_len))
+            print(2)
+
+            image_stream.seek(0)
+            image = Image.open(image_stream)
+            print(3)
+
+            try:
+                image.verify()
+            except:
+                logger.info("Image verification failed.. Skipping")
+                continue
+            print(4)
+            #image = Image.open(image_stream) #Reopen is neccesary after verify...
+            save_image(image_stream, collection, image_date)
+            if(telegram_count[collection] == TELEGRAM_SEND_INTERVAL):
+                telegram_count[collection] = 0
+                send_image(image_stream)
+            else:
+                telegram_count[collection] = telegram_count[collection]+1
+    finally:
+        file.close()
+        clientsocket.close()
+        logger.info("Connection from " + str(addr[0]) + " has ended.")
+
+
+def save_image(image_stream, collection, date):
+    dir = COLLECTION_PATH+"/"+collection
+    Path(dir).mkdir(parents=True, exist_ok=True)
+    date_str = datetime.utcfromtimestamp(date).strftime('%Y-%m-%d_%H:%M:%S')
+    path = dir+"/%s.jpeg" % date_str
+    image = Image.open(image_stream)
+    image.save(path)
+    logger.info("New image saved to %s" % path)
+
+
+def send_image(image_stream):
+
+    image = Image.open(image_stream)
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format=image.format)
+    imgByteArr = imgByteArr.getvalue()
+    bot.send_photo(CHAT_ID, imgByteArr)
+
+
+def main():
+    global bot
+    updater = Updater(TELEGRAM_BOT_API_KEY, use_context=True)
+    dp = updater.dispatcher
+    bot = updater.bot
+    job_queue = updater.job_queue
+    dp.add_handler(CommandHandler("getID", get_id))
+    updater.start_polling()
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', SERVER_PORT))
+    server_socket.listen(0)
+    while True:
+        logger.info("Starting to listen for Connections")
+        c, addr = server_socket.accept()
+        _thread.start_new_thread(on_new_client, (c, addr))
+
+
+main()
